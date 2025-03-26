88282116d659093c78be762551286bd61e9dfe98
G00394925 <G00394925@atu.ie>
2025-03-26 12:16:29 +0000

    Start work on the registration page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'register',
+    loadComponent: () => import('./register/register.page').then( m => m.RegisterPage)
+  },
 ];
