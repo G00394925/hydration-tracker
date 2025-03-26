0012440abbbfb7213b640aaee10a312ad3c3989a
Macdarach Carty Joyce <145470957+G00394925@users.noreply.github.com>
2025-03-26 19:44:51 +0000

    Added card and progress bar

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardContent, IonCardTitle } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { RoundProgressModule } from 'angular-svg-round-progressbar';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-tab1',
