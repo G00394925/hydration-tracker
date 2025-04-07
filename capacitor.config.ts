4f812887af8a99ce7d06e2eb168da24d831a881e
G00394925 <G00394925@atu.ie>
2025-04-07 19:04:08 +0100

    Fixed manifest issues

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'hydration-tracker',
+  appName: 'Hydration Tracker',
   webDir: 'www'
 };
 
