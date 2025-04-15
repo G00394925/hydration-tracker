b23baab00641533daf33557b6dbdb1c4f80b45c7
G00394925 <G00394925@atu.ie>
2025-04-15 18:46:15 +0100

    User can clear their own progress

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonContent, IonCard, IonCardContent, IonLabel, IonItem, IonToggle, IonSelect, IonSelectOption, IonCardTitle, IonCardHeader } from '@ionic/angular/standalone';
+import { IonContent, IonCard, IonCardContent, IonLabel, IonItem, IonToggle, IonSelect, IonSelectOption, IonCardTitle, IonCardHeader, IonIcon } from '@ionic/angular/standalone';
 import { StorageService } from '../storage.service';
 
 
@@ -7,12 +7,17 @@ import { StorageService } from '../storage.service';
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  imports: [IonCardHeader, IonCardTitle, IonToggle, IonItem, IonLabel, IonContent, IonCard, IonCardContent, IonSelect, IonSelectOption],
+  imports: [IonIcon, IonCardHeader, IonCardTitle, IonToggle, IonItem, IonLabel, IonContent, IonCard, IonCardContent, IonSelect, IonSelectOption],
 })
 
 export class Tab3Page {
   constructor(private storageService: StorageService) {}
 
+  resetProgress() {
+    this.storageService.set('currentProgress', 0);
+    this.storageService.set('todaysDrinks', 0);
+    this.storageService.set('lastDrink', '');
+  }
 
   clearAllData() {
     this.storageService.clear();
