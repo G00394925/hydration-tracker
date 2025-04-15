f2974fcc3f701de4b7fe2a1b7743d05bc198eaa7
G00394925 <G00394925@atu.ie>
2025-04-15 16:34:16 +0100

    Started work on settings page

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonLabel, IonListHeader, IonItem, IonInput, IonNote } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonNote, IonInput, IonItem, IonListHeader, IonLabel, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, ExploreContainerComponent, IonCardHeader, IonCardTitle],
 })
 export class Tab3Page {
   constructor() {}
