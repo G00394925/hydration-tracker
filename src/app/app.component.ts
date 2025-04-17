b0f53e930ec2983ecf34f5d97c3f8baba9b1000b
G00394925 <G00394925@atu.ie>
2025-04-17 14:04:45 +0100

    Service worker seems to work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 
 @Component({
@@ -8,4 +8,14 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 })
 export class AppComponent {
   constructor() {}
+
+  ngOnInit() {
+    if('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/ngsw-worker.js').then(() => {
+        console.log('Service Worker registered successfully');
+      }).catch((error) => {
+        console.error('Service Worker registration failed:', error);
+      });
+    }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { IonicStorageModule } from '@ionic/storage-angular';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { RouteReuseStrategy } from '@angular/router';
+import { provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { provideServiceWorker } from '@angular/service-worker';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -13,5 +15,9 @@ bootstrapApplication(AppComponent, {
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
     importProvidersFrom(IonicStorageModule.forRoot()),
-  ],
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: environment.production,
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+  ]
 });
